0475dd67fbe3f40c063335e1108447a72b38d18c
tylerachang98 <tylerachang98@gmail.com>
2018-01-25 15:17:48 -0600

    Heavily cleaned the code to make more readable

--- RemainderAlgorithms.cpp
+++ RemainderAlgorithms.cpp
@@ -1,7 +1,11 @@
 /*  Computes a number based on prime modulo values
  *  See Chinese Remainder Theorem for proof of solution existence
+ *  Implements and times three algorithms for computing the solution
+ *
+ *  Supports up to 15 primes
  *
  *  Originally written 2016-06-16
+ *  Updated for Math290 Reading in Number Theory
  *  
  */
 
@@ -13,106 +17,67 @@
 
 using namespace std;
 
-int calcValue();
 int loadPrimes();
 int loadAValues();
 
-unsigned long long testSpeed();
-unsigned long long testSpeed2();
+unsigned long long method1();
+unsigned long long method2();
+unsigned long long method3();
 
-unsigned long long n;
-unsigned long long interval;
-unsigned long long value;
 unsigned long long a[15];
 int mod[15];
-int primeLimit = 15;
 int prime[15];
 
 clock_t timer;
-
 string response;
+int primeLimit;
 
 int main() {
     loadPrimes();
-    loadAValues();
+    loadAValues(); //product of the first nth primes
 
     /*display a values
-     for (int c = 0; c < primeLimit; c++)
+     for (int c = 0; c < 15; c++)
      {
-     cout << c << ": " << a[c] << endl;
+        cout << c << ": " << a[c] << endl;
      }
      */
-
-    mod[0] = 1;
-    mod[1] = 2;
-    mod[2] = 2;
-    mod[3] = 3;
-    mod[4] = 1;
-    mod[5] = 11;
-    mod[6] = 13;
-    mod[7] = 17;
-    mod[8] = 19;
-    mod[9] = 23;
-    mod[10] = 29;
-    mod[11] = 31;
-    mod[12] = 37;
-    mod[13] = 41;
 
     while (true) {
         cout << "Mods: ";
         getline(cin, response);
-        stringstream(response) >> primeLimit;
-        for (int c = 0; c < primeLimit; c++) //c max = number of mods
-                {
+        stringstream(response) >> primeLimit; //how many primes
+        if (primeLimit > 15) {
+            cout << "Supports up to 15 primes.\n";
+            continue;
+        }
+        for (int c = 0; c < primeLimit; c++) {
             getline(cin, response);
             stringstream(response) >> mod[c];
         }
+        cout << endl;
 
         timer = clock();
-        calcValue();
-        cout << value;
+        cout << method1(); //brute force method
         timer = clock() - timer;
-        cout << "     " << timer << "  " << CLOCKS_PER_SEC << endl;
-
+        cout << "     Time: " << timer << endl;
+        
         timer = clock();
-        cout << testSpeed2();
+        cout << method2();
         timer = clock() - timer;
-        cout << "     " << timer << "  " << CLOCKS_PER_SEC << endl;
+        cout << "     Time: " << timer << endl;
+        
+        timer = clock();
+        cout << method3();
+        timer = clock() - timer;
+        cout << "     Time: " << timer << endl;
+        
+        cout << "(" << CLOCKS_PER_SEC << " clocks per second)" << endl;
 
+        //print the number to add to generate more solutions
         cout << " (" << a[primeLimit - 1] << ")" << endl << endl;
-        //cout << a[4] << endl; //a number = number of mods - 1
     }
     cin.get();
-    return 0;
-}
-
-int calcValue() {
-    int test = 0;
-
-    if (mod[0] == 0)
-        value = 0;
-    else
-        value = 1;
-    int y;
-    int x;
-    for (int c = 1; c < primeLimit; c++) //c max = number of mods
-            {
-        y = (a[c] + mod[c] - value) % prime[c];
-        x = a[c - 1] % prime[c];            //offset between prime mod and value
-        while (true)    //calculate which iteration it is based on offsets
-        {
-            if (y % x == 0) {
-                y = y / x;
-                break;
-            } else {
-                y = y + prime[c];
-                test = test + 1;
-            }
-        }
-
-        cout << "(" << test << ") ";
-        value = value + y * a[c - 1];
-    }
     return 0;
 }
 
@@ -137,13 +102,14 @@ int loadPrimes() {
 
 int loadAValues() {
     a[0] = 2;
-    for (int c = 1; c < primeLimit; c++) {
+    for (int c = 1; c < 15; c++) {
         a[c] = a[c - 1] * prime[c];
     }
     return 0;
 }
 
-unsigned long long testSpeed() {
+/* Calculates the value by brute force */
+unsigned long long method1() {
     unsigned long long n;
     for (n = 0; n < a[primeLimit - 1]; n++) {
         for (int n2 = 0; n2 < primeLimit; n2++) {
@@ -156,8 +122,13 @@ unsigned long long testSpeed() {
     return 0;
 }
 
-unsigned long long testSpeed2() {
+/* Calculates the value by a second method */
+unsigned long long method2() {
+    unsigned long long value;
+    
+    //keeps track of how many times value had to be incremented
     int test = 0;
+    
     if (mod[0] == 0)
         value = 0;
     else
@@ -167,8 +138,43 @@ unsigned long long testSpeed2() {
             value = value + a[n - 1];
             test = test + 1;
         }
-        cout << "(" << test << ") ";
+    }
+    
+    //print how many times test was incremented
+    //cout << "(" << test << ") ";
+    return value;
+}
+
+/* Calculates the value by a third method */
+unsigned long long method3() {
+    unsigned long long value;
+    
+    //keeps track of how many times value had to be incremented
+    int test = 0;
+    
+    if (mod[0] == 0)
+        value = 0;
+    else
+        value = 1;
+    int y;
+    int x;
+    for (int c = 1; c < primeLimit; c++) {
+        y = (a[c] + mod[c] - value) % prime[c];
+        x = a[c - 1] % prime[c]; //offset between prime mod and value
+        while (true) //calculate which iteration it is based on offsets
+        {
+            if (y % x == 0) {
+                y = y / x;
+                break;
+            } else {
+                y = y + prime[c];
+                test = test + 1;
+            }
+        }
+
+        //print how many times test was incremented
+        //cout << "(" << test << ") ";
+        value = value + y * a[c - 1];
     }
     return value;
-
 }
